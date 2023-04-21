1bf5af55ebc3c6f998613918d778aaf7c34b1bf0
andersthemagi <andres.sepulveda808@gmail.com>
2023-04-20 19:07:50 -0600

    project setup
    Ref: Initial Setup #1

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function App() {
+    return (
+        <h1>Eberron Cosmology Model</h1>
+    );
+};
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require("path");
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: __dirname + '/public/index.html',
+  filename: 'index.html',
+  inject: 'body'
+});
+
+module.exports = {
+    mode: "development",
+    entry: "./index.js",
+    output: {
+        path: path.resolve(__dirname, "public"),
+        filename: "main.js"
+    },
+    target: "web",
+    plugins: [HTMLWebpackPluginConfig],
+    devServer: {
+        port: "6969",
+        static: ["./public"],
+        open: true,
+        hot: true,
+        liveReload: true
+    },
+    resolve: {
+        extensions: [".js", ".jsx", ".json"]
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: "babel-loader"
+            }
+        ]
+    }
+}
